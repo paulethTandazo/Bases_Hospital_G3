ce1795934bf45f11d0efc2de319e3266c1530876
paulethTandazo <117050687+paulethTandazo@users.noreply.github.com>
2024-07-10 23:40:59 -0500

    creando entidad paciente

--- Base_Hospital.sql
+++ Base_Hospital.sql
@@ -1,2 +1,11 @@
 create database Base_Hospital;
 use Base_Hospital;
+CREATE TABLE PATIENT 
+    (Cedula         CHAR(10)     NOT NULL,
+     Nombre         VARCHAR(50)  NOT NULL,
+	 Apellido		VARCHAR(50)	 NOT NULL,
+     Edad           INT          NOT NULL,
+     Fcumpleanos    DATE         NOT NULL,
+     Direccion      VARCHAR(50)  NOT NULL,
+     PRIMARY KEY(Cedula));
+     
